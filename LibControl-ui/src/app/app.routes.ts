63aacba41d464240be890d86a310942ed2cd8e0b
Cordeiroovelha <cordeiro.juttel@gmail.com>
2026-09-17 20:26:41 -0300

    Pequenas mudanças no estilo da pagina e começo de implementação de metodos de segurança

--- LibControl-ui/src/app/app.routes.ts
+++ LibControl-ui/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BooksListComponent } from './books/books-list/books-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/login', pathMatch: 'full'},
+    {path: 'books', component: BooksListComponent}
+];
